9138058ca26c1b1983691fe27d4adb9a327b3119
mi5ha6in <mi5ha6in@gmail.com>
2021-10-22 01:23:29 +0300

    adds the simplest implementation of acceptUserMove function

--- src/Game.js
+++ src/Game.js
@@ -1,9 +1,17 @@
 export default class Game {
-  getState() {
-    return [
+  constructor() {
+    this._board = [
       ['', '', ''],
       ['', '', ''],
       ['', '', ''],
     ]
+  }
+
+  getState() {
+    return this._board
+  }
+
+  acceptUserMove(x, y) {
+    this._board[0][0] = 'x'
   }
 }
